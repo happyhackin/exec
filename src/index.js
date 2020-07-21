555115502c6b39c0866031cf975c9556225bfbc5
Duncan Marsh <slugbyte@slugbyte.com>
2020-07-22 02:49:54 +0800

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ module.exports.exec = (cmd, options={}) => {
   return new Promise((resolve, reject) => {
     const child = exec(cmd, options)
 
-
     let completed = false
     let logged = ""
     let stdout = ""
